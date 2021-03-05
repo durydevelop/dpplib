5b8f4ec0950436dc2ad89d06683f2e1dcea02d3a
Fabio Durigon <fabio@photorec.it>
2021-03-05 21:08:23 +0100

    More monolitic

--- build/CMakeFiles/DPPLIB.dir/DependInfo.cmake
+++ build/CMakeFiles/DPPLIB.dir/DependInfo.cmake
@@ -4,21 +4,21 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "E:/Programming/Lib/cpp/libdpp/lib/DCsv.cpp" "E:/Programming/Lib/cpp/libdpp/build/CMakeFiles/DPPLIB.dir/lib/DCsv.cpp.obj"
-  "E:/Programming/Lib/cpp/libdpp/lib/DPath.cpp" "E:/Programming/Lib/cpp/libdpp/build/CMakeFiles/DPPLIB.dir/lib/DPath.cpp.obj"
-  "E:/Programming/Lib/cpp/libdpp/lib/DPathWatcher.cpp" "E:/Programming/Lib/cpp/libdpp/build/CMakeFiles/DPPLIB.dir/lib/DPathWatcher.cpp.obj"
-  "E:/Programming/Lib/cpp/libdpp/lib/DPreferences.cpp" "E:/Programming/Lib/cpp/libdpp/build/CMakeFiles/DPPLIB.dir/lib/DPreferences.cpp.obj"
-  "E:/Programming/Lib/cpp/libdpp/lib/DShell.cpp" "E:/Programming/Lib/cpp/libdpp/build/CMakeFiles/DPPLIB.dir/lib/DShell.cpp.obj"
-  "E:/Programming/Lib/cpp/libdpp/lib/DString.cpp" "E:/Programming/Lib/cpp/libdpp/build/CMakeFiles/DPPLIB.dir/lib/DString.cpp.obj"
-  "E:/Programming/Lib/cpp/libdpp/lib/DStringGrid.cpp" "E:/Programming/Lib/cpp/libdpp/build/CMakeFiles/DPPLIB.dir/lib/DStringGrid.cpp.obj"
-  "E:/Programming/Lib/cpp/libdpp/lib/DSyncFile.cpp" "E:/Programming/Lib/cpp/libdpp/build/CMakeFiles/DPPLIB.dir/lib/DSyncFile.cpp.obj"
-  "E:/Programming/Lib/cpp/libdpp/lib/DSyncWatcher.cpp" "E:/Programming/Lib/cpp/libdpp/build/CMakeFiles/DPPLIB.dir/lib/DSyncWatcher.cpp.obj"
-  "E:/Programming/Lib/cpp/libdpp/lib/DUpdate.cpp" "E:/Programming/Lib/cpp/libdpp/build/CMakeFiles/DPPLIB.dir/lib/DUpdate.cpp.obj"
-  "E:/Programming/Lib/cpp/libdpp/lib/DWindow.cpp" "E:/Programming/Lib/cpp/libdpp/build/CMakeFiles/DPPLIB.dir/lib/DWindow.cpp.obj"
-  "E:/Programming/Lib/cpp/libdpp/lib/network/DNetwork.cpp" "E:/Programming/Lib/cpp/libdpp/build/CMakeFiles/DPPLIB.dir/lib/network/DNetwork.cpp.obj"
-  "E:/Programming/Lib/cpp/libdpp/lib/network/DRESTClient.cpp" "E:/Programming/Lib/cpp/libdpp/build/CMakeFiles/DPPLIB.dir/lib/network/DRESTClient.cpp.obj"
-  "E:/Programming/Lib/cpp/libdpp/lib/network/DUdpServer.cpp" "E:/Programming/Lib/cpp/libdpp/build/CMakeFiles/DPPLIB.dir/lib/network/DUdpServer.cpp.obj"
-  "E:/Programming/Lib/cpp/libdpp/lib/network/DUri.cpp" "E:/Programming/Lib/cpp/libdpp/build/CMakeFiles/DPPLIB.dir/lib/network/DUri.cpp.obj"
+  "E:/Dev/cpp/lib/libdpp/lib/DCsv.cpp" "E:/Dev/cpp/lib/libdpp/build/CMakeFiles/DPPLIB.dir/lib/DCsv.cpp.obj"
+  "E:/Dev/cpp/lib/libdpp/lib/DPath.cpp" "E:/Dev/cpp/lib/libdpp/build/CMakeFiles/DPPLIB.dir/lib/DPath.cpp.obj"
+  "E:/Dev/cpp/lib/libdpp/lib/DPathWatcher.cpp" "E:/Dev/cpp/lib/libdpp/build/CMakeFiles/DPPLIB.dir/lib/DPathWatcher.cpp.obj"
+  "E:/Dev/cpp/lib/libdpp/lib/DPreferences.cpp" "E:/Dev/cpp/lib/libdpp/build/CMakeFiles/DPPLIB.dir/lib/DPreferences.cpp.obj"
+  "E:/Dev/cpp/lib/libdpp/lib/DShell.cpp" "E:/Dev/cpp/lib/libdpp/build/CMakeFiles/DPPLIB.dir/lib/DShell.cpp.obj"
+  "E:/Dev/cpp/lib/libdpp/lib/DString.cpp" "E:/Dev/cpp/lib/libdpp/build/CMakeFiles/DPPLIB.dir/lib/DString.cpp.obj"
+  "E:/Dev/cpp/lib/libdpp/lib/DStringGrid.cpp" "E:/Dev/cpp/lib/libdpp/build/CMakeFiles/DPPLIB.dir/lib/DStringGrid.cpp.obj"
+  "E:/Dev/cpp/lib/libdpp/lib/DSyncFile.cpp" "E:/Dev/cpp/lib/libdpp/build/CMakeFiles/DPPLIB.dir/lib/DSyncFile.cpp.obj"
+  "E:/Dev/cpp/lib/libdpp/lib/DSyncWatcher.cpp" "E:/Dev/cpp/lib/libdpp/build/CMakeFiles/DPPLIB.dir/lib/DSyncWatcher.cpp.obj"
+  "E:/Dev/cpp/lib/libdpp/lib/DUpdate.cpp" "E:/Dev/cpp/lib/libdpp/build/CMakeFiles/DPPLIB.dir/lib/DUpdate.cpp.obj"
+  "E:/Dev/cpp/lib/libdpp/lib/DWindow.cpp" "E:/Dev/cpp/lib/libdpp/build/CMakeFiles/DPPLIB.dir/lib/DWindow.cpp.obj"
+  "E:/Dev/cpp/lib/libdpp/lib/network/DNetwork.cpp" "E:/Dev/cpp/lib/libdpp/build/CMakeFiles/DPPLIB.dir/lib/network/DNetwork.cpp.obj"
+  "E:/Dev/cpp/lib/libdpp/lib/network/DRESTClient.cpp" "E:/Dev/cpp/lib/libdpp/build/CMakeFiles/DPPLIB.dir/lib/network/DRESTClient.cpp.obj"
+  "E:/Dev/cpp/lib/libdpp/lib/network/DUdpServer.cpp" "E:/Dev/cpp/lib/libdpp/build/CMakeFiles/DPPLIB.dir/lib/network/DUdpServer.cpp.obj"
+  "E:/Dev/cpp/lib/libdpp/lib/network/DUri.cpp" "E:/Dev/cpp/lib/libdpp/build/CMakeFiles/DPPLIB.dir/lib/network/DUri.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
@@ -27,7 +27,6 @@ set(CMAKE_CXX_TARGET_INCLUDE_PATH
   "../lib"
   "../include"
   "."
-  "E:/Programming/Lib/cpp/boost"
   )
 
 # Targets to which this target links.
--- build/CMakeFiles/release.dir/build.make
+++ build/CMakeFiles/release.dir/build.make
@@ -62,10 +62,10 @@ RM = C:/msys64/mingw64/bin/cmake.exe -E rm -f
 EQUALS = =
 
 # The top-level source directory on which CMake was run.
-CMAKE_SOURCE_DIR = E:/Programming/Lib/cpp/libdpp
+CMAKE_SOURCE_DIR = E:/Dev/cpp/lib/libdpp
 
 # The top-level build directory on which CMake was run.
-CMAKE_BINARY_DIR = E:/Programming/Lib/cpp/libdpp/build
+CMAKE_BINARY_DIR = E:/Dev/cpp/lib/libdpp/build
 
 # Utility rule file for release.
 
@@ -73,9 +73,9 @@ CMAKE_BINARY_DIR = E:/Programming/Lib/cpp/libdpp/build
 include CMakeFiles/release.dir/progress.make
 
 CMakeFiles/release:
-	@$(CMAKE_COMMAND) -E cmake_echo_color --switch=$(COLOR) --blue --bold --progress-dir=E:/Programming/Lib/cpp/libdpp/build/CMakeFiles --progress-num=$(CMAKE_PROGRESS_1) "Creating the executable in the release mode."
-	C:/msys64/mingw64/bin/cmake.exe -DCMAKE_BUILD_TYPE=Release E:/Programming/Lib/cpp/libdpp
-	C:/msys64/mingw64/bin/cmake.exe --build E:/Programming/Lib/cpp/libdpp/build --target all
+	@$(CMAKE_COMMAND) -E cmake_echo_color --switch=$(COLOR) --blue --bold --progress-dir=E:/Dev/cpp/lib/libdpp/build/CMakeFiles --progress-num=$(CMAKE_PROGRESS_1) "Creating the executable in the release mode."
+	C:/msys64/mingw64/bin/cmake.exe -DCMAKE_BUILD_TYPE=Release E:/Dev/cpp/lib/libdpp
+	C:/msys64/mingw64/bin/cmake.exe --build E:/Dev/cpp/lib/libdpp/build --target all
 
 release: CMakeFiles/release
 release: CMakeFiles/release.dir/build.make
@@ -92,6 +92,6 @@ CMakeFiles/release.dir/clean:
 .PHONY : CMakeFiles/release.dir/clean
 
 CMakeFiles/release.dir/depend:
-	$(CMAKE_COMMAND) -E cmake_depends "Unix Makefiles" E:/Programming/Lib/cpp/libdpp E:/Programming/Lib/cpp/libdpp E:/Programming/Lib/cpp/libdpp/build E:/Programming/Lib/cpp/libdpp/build E:/Programming/Lib/cpp/libdpp/build/CMakeFiles/release.dir/DependInfo.cmake --color=$(COLOR)
+	$(CMAKE_COMMAND) -E cmake_depends "Unix Makefiles" E:/Dev/cpp/lib/libdpp E:/Dev/cpp/lib/libdpp E:/Dev/cpp/lib/libdpp/build E:/Dev/cpp/lib/libdpp/build E:/Dev/cpp/lib/libdpp/build/CMakeFiles/release.dir/DependInfo.cmake --color=$(COLOR)
 .PHONY : CMakeFiles/release.dir/depend
 
