842c4fbfa4bed2621cebe776afc4fe6ce68b6db2
durykiller <develop@dury.it>
2021-04-13 15:50:38 +0200

    mar 13 apr 2021, 15.50.38, CEST

---
+++ boost/CMakeFiles/DPPLIB.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/Dev/cpp/lib/libdpp/lib/DCsv.cpp" "/home/pi/Dev/cpp/lib/libdpp/boost/CMakeFiles/DPPLIB.dir/lib/DCsv.cpp.o"
+  "/home/pi/Dev/cpp/lib/libdpp/lib/DPath.cpp" "/home/pi/Dev/cpp/lib/libdpp/boost/CMakeFiles/DPPLIB.dir/lib/DPath.cpp.o"
+  "/home/pi/Dev/cpp/lib/libdpp/lib/DPathWatcher.cpp" "/home/pi/Dev/cpp/lib/libdpp/boost/CMakeFiles/DPPLIB.dir/lib/DPathWatcher.cpp.o"
+  "/home/pi/Dev/cpp/lib/libdpp/lib/DPreferences.cpp" "/home/pi/Dev/cpp/lib/libdpp/boost/CMakeFiles/DPPLIB.dir/lib/DPreferences.cpp.o"
+  "/home/pi/Dev/cpp/lib/libdpp/lib/DSettings.cpp" "/home/pi/Dev/cpp/lib/libdpp/boost/CMakeFiles/DPPLIB.dir/lib/DSettings.cpp.o"
+  "/home/pi/Dev/cpp/lib/libdpp/lib/DShell.cpp" "/home/pi/Dev/cpp/lib/libdpp/boost/CMakeFiles/DPPLIB.dir/lib/DShell.cpp.o"
+  "/home/pi/Dev/cpp/lib/libdpp/lib/DString.cpp" "/home/pi/Dev/cpp/lib/libdpp/boost/CMakeFiles/DPPLIB.dir/lib/DString.cpp.o"
+  "/home/pi/Dev/cpp/lib/libdpp/lib/DStringGrid.cpp" "/home/pi/Dev/cpp/lib/libdpp/boost/CMakeFiles/DPPLIB.dir/lib/DStringGrid.cpp.o"
+  "/home/pi/Dev/cpp/lib/libdpp/lib/DSyncFile.cpp" "/home/pi/Dev/cpp/lib/libdpp/boost/CMakeFiles/DPPLIB.dir/lib/DSyncFile.cpp.o"
+  "/home/pi/Dev/cpp/lib/libdpp/lib/DSyncWatcher.cpp" "/home/pi/Dev/cpp/lib/libdpp/boost/CMakeFiles/DPPLIB.dir/lib/DSyncWatcher.cpp.o"
+  "/home/pi/Dev/cpp/lib/libdpp/lib/DUpdate.cpp" "/home/pi/Dev/cpp/lib/libdpp/boost/CMakeFiles/DPPLIB.dir/lib/DUpdate.cpp.o"
+  "/home/pi/Dev/cpp/lib/libdpp/lib/DWindow.cpp" "/home/pi/Dev/cpp/lib/libdpp/boost/CMakeFiles/DPPLIB.dir/lib/DWindow.cpp.o"
+  "/home/pi/Dev/cpp/lib/libdpp/lib/network/DNetwork.cpp" "/home/pi/Dev/cpp/lib/libdpp/boost/CMakeFiles/DPPLIB.dir/lib/network/DNetwork.cpp.o"
+  "/home/pi/Dev/cpp/lib/libdpp/lib/network/DRESTClient.cpp" "/home/pi/Dev/cpp/lib/libdpp/boost/CMakeFiles/DPPLIB.dir/lib/network/DRESTClient.cpp.o"
+  "/home/pi/Dev/cpp/lib/libdpp/lib/network/DUdpServer.cpp" "/home/pi/Dev/cpp/lib/libdpp/boost/CMakeFiles/DPPLIB.dir/lib/network/DUdpServer.cpp.o"
+  "/home/pi/Dev/cpp/lib/libdpp/lib/network/DUri.cpp" "/home/pi/Dev/cpp/lib/libdpp/boost/CMakeFiles/DPPLIB.dir/lib/network/DUri.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/pi/Dev/cpp/lib/boost"
+  "../lib"
+  "../include"
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ boost/CMakeFiles/DPPLIB.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DPPLIB.dir/lib/DCsv.cpp.o"
+  "CMakeFiles/DPPLIB.dir/lib/DPath.cpp.o"
+  "CMakeFiles/DPPLIB.dir/lib/DPathWatcher.cpp.o"
+  "CMakeFiles/DPPLIB.dir/lib/DPreferences.cpp.o"
+  "CMakeFiles/DPPLIB.dir/lib/DSettings.cpp.o"
+  "CMakeFiles/DPPLIB.dir/lib/DShell.cpp.o"
+  "CMakeFiles/DPPLIB.dir/lib/DString.cpp.o"
+  "CMakeFiles/DPPLIB.dir/lib/DStringGrid.cpp.o"
+  "CMakeFiles/DPPLIB.dir/lib/DSyncFile.cpp.o"
+  "CMakeFiles/DPPLIB.dir/lib/DSyncWatcher.cpp.o"
+  "CMakeFiles/DPPLIB.dir/lib/DUpdate.cpp.o"
+  "CMakeFiles/DPPLIB.dir/lib/DWindow.cpp.o"
+  "CMakeFiles/DPPLIB.dir/lib/network/DNetwork.cpp.o"
+  "CMakeFiles/DPPLIB.dir/lib/network/DRESTClient.cpp.o"
+  "CMakeFiles/DPPLIB.dir/lib/network/DUdpServer.cpp.o"
+  "CMakeFiles/DPPLIB.dir/lib/network/DUri.cpp.o"
+  "libDPPLIB.a"
+  "libDPPLIB.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DPPLIB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
