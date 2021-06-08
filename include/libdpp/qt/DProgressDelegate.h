de91a799afc3f5f8df6b726570cc16bb0ebb52ad
Fabio Durigon <fabio@photorec.it>
2021-06-08 19:29:57 +0200

    QItemDelegate

---
+++ include/libdpp/qt/DProgressDelegate.h
@@ -0,0 +1,19 @@
+#ifndef DProgressDelegateH
+#define DProgressDelegateH
+
+#ifdef QT_GUI_LIB
+
+#include <QItemDelegate>
+
+class DProgressDelegate : public QItemDelegate
+{
+    Q_OBJECT
+    public:
+        DProgressDelegate(QObject *parent = nullptr);
+        void setColumnIndex(int index);
+        void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const override;
+    private:
+        int columnIndex;
+};
+#endif
+#endif
